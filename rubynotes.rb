a7e8e7df3d886cb61f66583f98f620687bdd5ff4
MickAvery <mav.cuyugan@gmail.com>
2016-09-13 11:01:36 -0700

    started integrating curses gem into application, now about to save notes into database

--- rubynotes.rb
+++ rubynotes.rb
@@ -1,83 +1,142 @@
 require 'sqlite3'
-require 'colorize'
+require 'curses'
+include Curses
 #require 'debug'
 
-def clear
-  system "clear" or system "cls"
+# Initialize curses and allow for color in text
+init_screen
+start_color
+#nocbreak
+
+# Setup global window variables
+@menu
+@border
+@note_border
+@note
+@db
+
+def setup_menu(ask_input)
+  @menu = Curses::Window.new(lines - 3, cols - 3, 2, 2)
+  init_pair(1, 1, 0)
+  init_pair(2, 2, 0)
+  init_pair(3, 3, 0)
+  @menu.attrset(color_pair(1))
+  @menu.addstr("Welcome to Ruby Notes!\n\n\n")
+  
+  @menu.attrset(color_pair(2))
+  @menu.addstr("n) Make a new note\n")
+  
+  @menu.attrset(color_pair(3))
+  @menu.addstr("q) Quit Ruby Notes\n")
+  
+  @menu.attroff(color_pair(3))
+  @menu.refresh
+  
+  if ask_input
+    @menu.addstr("Action: ")
+    @menu.refresh
+    input = @menu.getch
+    
+    case input
+    when "q"
+      close_screen
+      abort
+    when "n"
+      @menu.addstr("\nYou chose to open notes window!")
+      @menu.refresh
+      sleep 1
+      new_note()
+    else
+      @menu.addstr("\nInvalid input")
+      @menu.refresh
+      sleep 2
+      close_screen
+      abort
+    end       
+  end
 end
 
-def new_note(db)
-  clear()
-  puts "Title:"        # make red or something
-  title = gets
-  #puts
-  #puts "========================"
+def setup_border
+  @border = Curses::Window.new(lines, cols, 0, 0)
+  @border.box("#", "#")
+  @border.refresh
+end
 
-  puts "Content:\n(opens Vim to allow you to input large amounts of text)"  # make orange or something
-  key_input = gets
-  if key_input == "\n"
-    system("vim", "/tmp/temp_content.txt")
-    content = `less /tmp/temp_content.txt`
-    `rm /tmp/temp_content.txt`
+def start_app
+  setup_border()
+  setup_menu(true)
+end
+
+def new_note
+  @note_border = Curses::Window.new(lines - 5, cols - 5, 4, 4)
+  @note_border.box("||", "=")
+  @note_border.refresh
+  
+  @note = Curses::Window.new(lines - 7, cols - 7, 6, 6)
+  @note.addstr("Title: ")
+  @note.refresh
+  title = @note.getstr
+
+  @note.addstr("\n")
+  @note.addstr("Content: \n")
+  @note.addstr("(opens Vim to allow you to input large amounts of text)")
+  @note.refresh
+  key_input = @note.getch
+  system("vim", "/tmp/temp_content.txt")
+  file_content = File.open("/tmp/temp_content.txt", "rb")
+  # TODO: check if file exists (aka if user actually put in something)
+  content = file_content.read
+  file_content.close
+  File.delete("/tmp/temp_content.txt")
+  setup_border()
+  setup_menu(false)
+  
+  # repopulate note_border and note window (TODO: find better, more generic, way of doing this)
+  @note_border.box("|", "-")
+  @note_border.refresh
+  @note.setpos(0, 0)
+  @note.addstr("Title: #{title}\n\n")
+  @note.addstr("Content: ")
+  @note.addstr("\n" + content + "\n\n")
+  @note.addstr("Tags (separated with commas \',\'): ")
+  tags = @note.getstr
+  tags.split(",").each do |tag|
+    @note.addstr("\n#{tag.strip}")
   end
-  #puts
-  #puts "========================"
-
-  puts "Tags:"         # make green or something
-  tag = gets
-  #puts
+  @note.refresh
+  sleep 3
 
   # create new note in table
-  db.execute("INSERT INTO Notes VALUES(?, ?, ?)", [@null, title, content])
-  noteId = db.get_first_value("SELECT last_insert_rowid()")
+  #@db.execute("INSERT INTO Notes VALUES(?, ?, ?)", [@null, title, content])
+  #noteId = @db.get_first_value("SELECT last_insert_rowid()")
 
   # if tag doesnt exist, create one
-  if db.execute("SELECT * FROM Tags WHERE tag_name=\'#{tag}\'").length <= 0
-    db.execute("INSERT INTO Tags VALUES(?, ?)", [@null, tag])
-    tagId = db.get_first_value("SELECT last_insert_rowid()")
-  else
-    tagId = db.get_first_value("SELECT id FROM Tags WHERE tag_name=\'#{tag}\'")
-  end
+  #if @db.execute("SELECT * FROM Tags WHERE tag_name=\'#{tag}\'").length <= 0
+  #  @db.execute("INSERT INTO Tags VALUES(?, ?)", [@null, tag])
+  #  tagId = @db.get_first_value("SELECT last_insert_rowid()")
+  #else
+  #  tagId = @db.get_first_value("SELECT id FROM Tags WHERE tag_name=\'#{tag}\'")
+  #end
   # create relationship between note and tag
-  db.execute("INSERT INTO NotesTags VALUES(?, ?, ?)", [@null, noteId, tagId])
+  #@db.execute("INSERT INTO NotesTags VALUES(?, ?, ?)", [@null, noteId, tagId])
 end
 
-def setup_menu
-  clear()
-  puts "n) Make a new note".cyan
-  puts "q) Quit Ruby Notes".light_red
-  print "Action: "
-end
-
-db = SQLite3::Database.new 'NotesDB.db'
+@db = SQLite3::Database.new 'NotesDB.@db'
 # check if NotesDB.Notes table exists
-unless db.execute("SELECT * FROM sqlite_master WHERE type=\'table\' AND name=\'Notes\'").length > 0
-  db.execute("CREATE TABLE Notes(id INTEGER PRIMARY KEY, 
+unless @db.execute("SELECT * FROM sqlite_master WHERE type=\'table\' AND name=\'Notes\'").length > 0
+  @db.execute("CREATE TABLE Notes(id INTEGER PRIMARY KEY, 
                                  title NVARCHAR, 
                                  content NVARCHAR)")
-  db.execute("CREATE TABLE Tags(id INTEGER PRIMARY KEY, 
+  @db.execute("CREATE TABLE Tags(id INTEGER PRIMARY KEY, 
                                 tag_name NVARCHAR)")
-  db.execute("CREATE TABLE NotesTags(id INTEGER PRIMARY KEY, 
+  @db.execute("CREATE TABLE NotesTags(id INTEGER PRIMARY KEY, 
                                      noteId INTEGER,
                                      tagId INTEGER,
                                      FOREIGN KEY(noteId) REFERENCES Notes(id),
                                      FOREIGN KEY(tagId) REFERENCES Tags(id))")
 end
 
-setup_menu()
+start_app()
 
-loop do
-  input = gets.chomp   
-  case input
-  when "n"
-    new_note(db)
-    setup_menu()
-  when "q"
-    clear()
-    abort
-  else
-    puts "Invalid entry, please try again"
-  end
-end
-
-# output UI stuff
+close_screen
+abort
